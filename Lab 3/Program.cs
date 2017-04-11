e7cd0d6af17db913f13aaa3ba1aece16df990f05
BrettMerri <the.b.merri@gmail.com>
2017-04-11 15:05:30 -0400

    initial commit

---
+++ Lab 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Learn your squares and cubes!\n");
+                Console.Write("Enter an integer: ");
+
+                string input = Console.ReadLine();
+
+                int inputNumber;
+                if (!int.TryParse(input, out inputNumber))
+                {
+                    Console.WriteLine("Input not a number");
+                }
+
+                Console.WriteLine("\nNumber\tSquared\tCubed");
+                Console.WriteLine("=======\t=======\t======");
+                Console.WriteLine(inputNumber);
+            }
+        }
+    }
+}
