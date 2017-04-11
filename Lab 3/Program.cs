75322a2925e8f59eadcd3b06067fafe2399c6749
BrettMerri <the.b.merri@gmail.com>
2017-04-11 15:35:05 -0400

    Added red console text to errors. Added documentation

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -10,33 +10,34 @@ namespace Lab_3
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            while (true)
+            Console.ForegroundColor = ConsoleColor.Cyan; //Change color of console text to Cyan
+            while (true) //Infinite loop
             {
                 Console.Clear();
                 Console.WriteLine("Learn your squares and cubes!\n");
                 Console.Write("Enter an integer: ");
 
-                string input = Console.ReadLine();
+                string input = Console.ReadLine(); //Prompts user for integer input
 
                 int inputNumber;
-                if (!int.TryParse(input, out inputNumber))
+                if (!int.TryParse(input, out inputNumber)) //Validating user input to ensure it is an integer
                 {
-                    Console.WriteLine("Input not a number");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: Input not a number");
                     return;
                 }
 
-                Console.WriteLine("\nNumber\t\tSquared\t\tCubed");
+                Console.WriteLine("\nNumber\t\tSquared\t\tCubed"); //Creating a table header in the console using tabs
                 Console.WriteLine("=======\t\t=======\t\t======");
-                for (int i = 1; i<=inputNumber; i++)
+
+                for (int i = 1; i<=inputNumber; i++) //Creates the numbers in the table
                 {
                     double squared = Math.Pow(i, 2);
                     double cubed = Math.Pow(i, 3);
                     Console.WriteLine("{0}\t\t{1}\t\t{2}", i, squared, cubed);
                 }
 
-                Console.Write("\nContinue? (y/n): ");
+                Console.Write("\nContinue? (y/n): "); //Prompts user if they want to continue
                 string continueInput = Console.ReadLine().ToLower();
 
                 if (continueInput == "y")
@@ -49,7 +50,8 @@ namespace Lab_3
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input!");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: Invalid input!");
                     return;
                 }
             }
